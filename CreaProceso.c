da3e50a64bace261af1a0ee7bcc7036bb950682f
Enrique Yoab Cortez Rios <yobi_jovi@hotmail.com>
2023-09-23 15:55:40 -0600

    Crear Proceso y la estructura del proceso listo

--- CreaProceso.c
+++ CreaProceso.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <unistd.h>
 
-struct Nodo{
-    char nombre;
-    struct nodo *siguiente; 
+struct proceso{
+    char nombre[];
+    int identificador;
+    int instruccion;
+    struct proceso *siguiente; 
+}
+
+struct proceso *agregar (struct proceso *cola, char *name,int id_proceso,int numInstr){
+    if(cola != NULL){
+
+    }else{
+
+    }
+}
+
+int main(){
+    struct proceso *padre=NULL;
 }
