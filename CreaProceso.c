56deeb09f6035354f4b37ec48400baa546e6183c
Enrique Yoab Cortez Rios <yobi_jovi@hotmail.com>
2023-09-23 22:37:22 -0600

    ultima actualizacion 10:37

--- CreaProceso.c
+++ CreaProceso.c
@@ -20,15 +20,16 @@ void crearCola(Cola *cola){ //solo inicializa la cola de los procesos
     cola->inicio=NULL;
 }
 
-Proceso *agregarProceso(Cola *cola,int id){   //agrega el proceso a la cola
+Proceso *agregarProceso(Cola *cola,int numProceso){   //agrega el proceso a la cola
     Proceso *nuevo_proceso = (Proceso*)malloc(sizeof(Proceso));
-    nuevo_proceso->posicion=id;
-    nuevo_proceso->identificador=10 + rand()%(31-10); //numero de instrucciones dado por el programa entre 10 y 30
+    nuevo_proceso->posicion=numProceso;
+    nuevo_proceso->identificador=rand()%1000;
+    nuevo_proceso->instruccion=10 + rand()%(31-10); //numero de instrucciones dado por el programa entre 10 y 30
     nuevo_proceso->siguiente=NULL;
 
-    printf("Ingrese el nombre del proceso %d: ",id);
+    printf("Ingrese el nombre del proceso %d: ",numProceso);
     scanf("%29s",nuevo_proceso->nombre); //limitar la entrada a 29 caracteres
-    if(cola!=NULL){
+    if(cola->inicio!=NULL){
         cola->inicio= nuevo_proceso;
         cola->final = nuevo_proceso;
     }else{
@@ -36,9 +37,38 @@ Proceso *agregarProceso(Cola *cola,int id){   //agrega el proceso a la cola
         cola->final=nuevo_proceso;
     }
 }
+Proceso *quitarProceso(Cola *cola){
+    if(!cola->inicio){
+        return NULL;
+    }
+    Proceso *proceso_removido=cola->inicio;
+    cola->inicio=cola->inicio->siguiente;
+    if(!cola->inicio){
+        cola->final=NULL;
+    }
+    return proceso_removido;
+}
+void visualizarCola(Cola *cola){
+    Proceso *temp=cola->inicio;
+    if(!temp){
+        printf("La cola esta vacia\n");
+        return;
+    }
+    printf("Proceso %d en la cola:\n",temp->posicion);
+    while(temp){
+        printf("ID: %d , Nombre: %s , Numero de instrucciones: %d",temp->identificador,temp->nombre,temp->instruccion);
+        temp = temp->siguiente;
+    }
+}
 
-
-int main(){
-   srand(time(NULL));
-
+void main(){
+    srand(time(NULL));
+    Cola cola;
+    crearCola(&cola); //se inicializa la cola de los procesos
+    int numeroProc;
+    printf("¿Cuantos procesos desea crear?: ");
+    scanf("%d",&numeroProc);
+    for(int i=1;i<=numeroProc;i++){
+        agregarProceso(&cola,i);
+    }
 }
